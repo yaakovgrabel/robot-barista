904968c354d7ccee444addbd703e78ada23aaf6f
YGrabel <yaakovgrabel@gmail.com>
2025-11-26 10:01:37 -0500

    spacing change

--- robot breista.py
+++ robot breista.py
@@ -13,12 +13,14 @@ if name == "ben" or name == "BEN" or name == "Ben":
     print("oh so you are one of those good ben's i see.... well welcome " + name + "\n\n\n")
 else:
     print("Hello " + name +" ,\nthank you so much for coming in today\n\n\n")
+
+
 #menu display
 menu = "black coffee, cappuccino, espresso, latte, frappuccino"
 
 print(name + " , what would you like form our menu? here is what we are serving.\n"
 + menu)
-#pricing 
+#pricing and order taking
 
 order = input()
 
@@ -32,7 +34,8 @@ elif order == "espresso":
     price = 5
 elif order == "latte":
     price = 9
-else:input("sorry we dont have that here, would you like somthing else from the menu? here is what we are serving\n" + menu + "\n")
+else:
+    input("sorry we dont have that here, would you like somthing else from the menu? here is what we are serving\n" + menu + "\n")
 
 
 price = 8
